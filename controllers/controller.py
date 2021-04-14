3fd827aca686c8e5c46391ba37606436f276f057
crispymccoy145 <samspam19941@gmail.com>
2021-04-14 17:45:08 +0100

    final commit of calculator homework

---
+++ controllers/controller.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add/<num_1>/<num_2>')
+def add(num_1, num_2):
+    return render_template('add.html', num_1 = int(num_1), num_2 = int(num_2), answer = int(num_1) + int(num_2))
+
+@app.route('/divide/<num_1>/<num_2>')
+def divide(num_1, num_2):
+    return render_template('divide.html', num_1 = int(num_1), num_2 = int(num_2), answer = int(num_1) / int(num_2))
+
+@app.route('/multiply/<num_1>/<num_2>')
+def multiply(num_1, num_2):
+    return render_template('multiply.html', num_1 = int(num_1), num_2 = int(num_2), answer = int(num_1) * int(num_2))
+
+@app.route('/subtract/<num_1>/<num_2>')
+def subtract(num_1, num_2):
+    return render_template('subtract.html', num_1 = int(num_1), num_2 = int(num_2), answer = int(num_1) - int(num_2))
+
